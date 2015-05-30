1813fb39590566b89007cf54558f7c60927a7223
Alexander Chernyakhovsky <achernya@mit.edu>
2015-05-30 19:09:34 -0400

    Fix identation

--- assets/webathena-sql/core.js
+++ assets/webathena-sql/core.js
@@ -1,43 +1,43 @@
 function supportsLocalStorage() {
     try {
-	return 'localStorage' in window && window['localStorage'] !== null;
+        return 'localStorage' in window && window['localStorage'] !== null;
     } catch (e) {
-	return false;
+        return false;
     }
 }
 
 function getCachedTicket() {
     if (!supportsLocalStorage()) {
-	return;
+        return;
     }
     var storedTicket = localStorage["webathena.ticket"];
     if (!storedTicket) {
-	return;
+        return;
     }
     var session = krb.Session.fromDict(JSON.parse(storedTicket));
     if (session.isExpired()) {
-	return;
+        return;
     }
     return session;
 }
 
 function saveTicketToCache(ticket) {
     if (!supportsLocalStorage()) {
-	return;
+        return;
     }
     localStorage["webathena.ticket"] = JSON.stringify(ticket);
 }
 
 function destroyCachedTicket() {
     if (!supportsLocalStorage()) {
-	return;
+        return;
     }
     localStorage.removeItem("webathena.ticket");
 }
 
 function getCurrentLocker() {
     if (!supportsLocalStorage()) {
-	return;
+        return;
     }
     // TODO implement this to actually work...
     return getCachedTicket().client.principalName.nameString[0];
@@ -47,29 +47,29 @@ function getTicket() {
     var deferred = Q.defer();
     var session = getCachedTicket();
     if (session) {
-	deferred.resolve(session);
-	return deferred.promise;
+        deferred.resolve(session);
+        return deferred.promise;
     }
     WinChan.open({
-	url: "https://webathena.mit.edu/#!request_ticket_v1",
-	relay_url: "https://webathena.mit.edu/relay.html",
-	params: {
-	    realm: 'ATHENA.MIT.EDU',
-	    principal: ['host', 'sql.mit.edu'],
-	}
+        url: "https://webathena.mit.edu/#!request_ticket_v1",
+        relay_url: "https://webathena.mit.edu/relay.html",
+        params: {
+            realm: 'ATHENA.MIT.EDU',
+            principal: ['host', 'sql.mit.edu'],
+        }
     }, function (err, r) {
-	console.log("got reply", err, r);
-	if (err) {
-	    deferred.reject(err);
-	    return;
-	}
-	if (r.status !== "OK") {
-	    deferred.reject(r);
-	    return;
-	}
-	saveTicketToCache(r.session);
-	session = krb.Session.fromDict(r.session);
-	deferred.resolve(session);
+        console.log("got reply", err, r);
+        if (err) {
+            deferred.reject(err);
+            return;
+        }
+        if (r.status !== "OK") {
+            deferred.reject(r);
+            return;
+        }
+        saveTicketToCache(r.session);
+        session = krb.Session.fromDict(r.session);
+        deferred.resolve(session);
     });
     return deferred.promise;
 }
@@ -81,8 +81,8 @@ function updateLoginControls() {
     notSignedIn.hidden = !!session;
     myAccount.hidden = !session;
     if (session) {
-	var username = document.getElementById("sql-username");
-	username.textContent = session.client.principalName.nameString[0];
+        var username = document.getElementById("sql-username");
+        username.textContent = session.client.principalName.nameString[0];
     }
 }
 
@@ -100,7 +100,7 @@ document.getElementById("log-out").addEventListener("click", function (e) {
 
 !function ($) {
     $(function() {
-	updateLoginControls();
+        updateLoginControls();
     })
 }(window.jQuery)
 
@@ -122,23 +122,23 @@ function remctl(command) {
     var streams = { };
 
     function flushStreams() {
-	var overallLength = 0;
-	for (var i = 0; (i < chunks.length); i++) {
-	    overallLength += chunks[i].length;
-	}
-	var arr = new Uint8Array(overallLength);
-	overallLength = 0;
-	for (var i = 0; (i < chunks.length); i++) {
-	    arr.set(chunks[i], overallLength);
-	    overallLength += chunks[i].length;
-	}
-	return arrayutils.toString(arr);
+        var overallLength = 0;
+        for (var i = 0; (i < chunks.length); i++) {
+            overallLength += chunks[i].length;
+        }
+        var arr = new Uint8Array(overallLength);
+        overallLength = 0;
+        for (var i = 0; (i < chunks.length); i++) {
+            arr.set(chunks[i], overallLength);
+            overallLength += chunks[i].length;
+        }
+        return arrayutils.toString(arr);
     }
 
     var chunks = [ ];
     return session.ready().then(function() {
         return session.command(command, function(stream, data) {
-	    chunks.push(data);
+            chunks.push(data);
         });
     }).then(function(status) {
         if (status !== 0 && status !== 1) {
@@ -175,14 +175,14 @@ function showAlert(basename, heading, body, style) {
     var alertTemplate = $("#alert-template");
     var alertPlaceholder = $("#" + basename + "-placeholder");
     if (alertPlaceholder.length > 0) {
-	var alertNode = alertTemplate.clone().removeAttr("id");
+        var alertNode = alertTemplate.clone().removeAttr("id");
         alertNode.find(".alert-template-head").text(heading);
         alertNode.find(".alert-template-body").text(body);
         alertNode.removeAttr("hidden");
-	if (style) {
+        if (style) {
             alertNode.addClass(style);
-	}
-	alertPlaceholder.append(alertNode);
+        }
+        alertPlaceholder.append(alertNode);
     }
 }
 
@@ -193,33 +193,33 @@ function clearAlerts(basename) {
 function registerModalListeners() {
     var cpw = $('#change-password');
     if (cpw) {
-	cpw.unbind("submit");
-	cpw.submit(function (e) {
-	    e.preventDefault();
+        cpw.unbind("submit");
+        cpw.submit(function (e) {
+            e.preventDefault();
             clearAlerts("password-alert");
-	    pw = cpw.find(".field-password").val();
-	    confirmPw = cpw.find(".field-confirmPassword").val()
-	    if (pw.length < 6) {
-		showAlert("password-alert", "Error!", "Password is too short.", "alert-error");
-		return false;
-	    }
-	    if (pw === confirmPw) {
-		showAlert("password-alert", "Processing!", "Please wait.");
+            pw = cpw.find(".field-password").val();
+            confirmPw = cpw.find(".field-confirmPassword").val()
+            if (pw.length < 6) {
+                showAlert("password-alert", "Error!", "Password is too short.", "alert-error");
+                return false;
+            }
+            if (pw === confirmPw) {
+                showAlert("password-alert", "Processing!", "Please wait.");
                 cpw.find(":submit").attr("disabled", "");
-		sqlCommand(["password", "set", getCurrentLocker(), pw]).finally(function () {
+                sqlCommand(["password", "set", getCurrentLocker(), pw]).finally(function () {
                     clearAlerts("password-alert");
                     cpw.find(":submit").removeAttr("disabled");
                 }).then(function (result) {
-		    showAlert("password-alert", "Success!", "Password updated.", "alert-success");
-		}, function (err) {
+                    showAlert("password-alert", "Success!", "Password updated.", "alert-success");
+                }, function (err) {
                     showAlert("password-alert", "Error", "Failed to change password: " + err, "alert-error");
                     throw err;
                 }).done();
-		return false;
-	    }
-	    showAlert("password-alert", "Error", "Passwords do not match.", "alert-error");
-	    return false;
-	});
+                return false;
+            }
+            showAlert("password-alert", "Error", "Passwords do not match.", "alert-error");
+            return false;
+        });
     }
     var profile = $('#change-profile');
     if (profile) {
@@ -227,7 +227,7 @@ function registerModalListeners() {
         profile.find("input").attr("disabled", "");
         profile.find(":submit").attr("disabled", "");
 
-	showAlert("profile-alert", "Loading...", "Please wait.");
+        showAlert("profile-alert", "Loading...", "Please wait.");
 
         sqlCommand(["profile", "get", getCurrentLocker()]).finally(function () {
             clearAlerts("profile-alert");
@@ -241,8 +241,8 @@ function registerModalListeners() {
             profile.find(".field-user-email").val(result.email);
 
             // NOW hook up the form.
-	    profile.submit(function (e) {
-	        e.preventDefault();
+            profile.submit(function (e) {
+                e.preventDefault();
                 clearAlerts("profile-alert");
 
                 var fullname = profile.find(".field-user-fullname").val();
@@ -252,24 +252,24 @@ function registerModalListeners() {
                     email: email
                 });
 
-		showAlert("profile-alert", "Processing!", "Please wait.");
+                showAlert("profile-alert", "Processing!", "Please wait.");
                 cpw.find(":submit").attr("disabled", "");
-		sqlCommand(["profile", "set", getCurrentLocker(), profileStr]).finally(function () {
+                sqlCommand(["profile", "set", getCurrentLocker(), profileStr]).finally(function () {
                     clearAlerts("profile-alert");
                     cpw.find(":submit").removeAttr("disabled");
                 }).then(function (result) {
-		    showAlert("profile-alert", "Success!", "Profile updated.", "alert-success");
-		}, function (err) {
+                    showAlert("profile-alert", "Success!", "Profile updated.", "alert-success");
+                }, function (err) {
                     showAlert("profile-alert", "Error", "Failed to change profile: " + err, "alert-error");
                     throw err;
                 }).done();
-		return false;
-	    });
+                return false;
+            });
         }, function (err) {
             showAlert("profile-alert", "Error", "Failed to get profile: " + err, "alert-error");
         }).done();
 
-	profile.unbind("submit");
+        profile.unbind("submit");
     }
 }
 
